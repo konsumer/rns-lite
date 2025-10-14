465335ee2780b3159cd7480f513c09f49ec737f4
David Konsumer <konsumer@jetboystudio.com>
2025-10-13 21:03:08 -0700

    simplify websocket-example

--- example_websocket.py
+++ example_websocket.py
@@ -18,6 +18,7 @@ ratchet_pub = rns.ratchet_get_public(ratchet)
 # I track annnounces, so I can send a message using their ratchet-pub (in their own ANNOUNCE)
 announces = {}
 
+# called periodically to ANNOUNCE myself
 async def periodic_announce(websocket, interval=30):
     while True:
         try:
@@ -28,55 +29,69 @@ async def periodic_announce(websocket, interval=30):
         except ConnectionClosed:
             break
 
+
+async def handle_announce(packet):
+    print(f"ANNOUNCE from {packet['destination_hash'].hex()}")
+    announce = rns.announce_parse(packet)
+    if announce['valid']:
+        dest = packet['destination_hash']
+        announces[packet['destination_hash']] = announce
+        announces[packet['destination_hash']]['destination_hash'] = packet['destination_hash']
+        print(f"  Saved ({len(announces)}) announce from {packet['destination_hash'].hex()}")
+
+
+async def handle_proof(packet):
+    print(f"PROOF for message {packet['destination_hash'].hex()}")
+    ## TODO: check proof?
+
+async def handle_data(packet, websocket):
+    message_id = rns.get_message_id(packet)
+    print(f"DATA ({message_id.hex()}) for {packet['destination_hash'].hex()}")
+    
+    # it was for me?
+    if packet['destination_hash'] == me_dest:
+        # tell them I got it
+        print(f"sending PROOF ({message_id.hex()})")
+        await websocket.send(rns.build_proof(me, packet, message_id))
+
+        # Decrypt the message
+        plaintext = rns.message_decrypt(packet, me, [ratchet])
+        if plaintext:
+            message = rns.parse_lxmf_message(plaintext)
+            content_text = message['content'].decode('utf-8')
+            print(f"  From: {message['source_hash'].hex()}")
+            print(f"  Title: {message['title'].decode('utf-8')}")
+            print(f"  Content: {content_text}")
+
+            # Send reply to sender
+            if message['source_hash'] in announces:
+                reply = {
+                    'title': 'EchoBot',
+                    'content': content_text
+                } 
+                await websocket.send(rns.build_lxmf_message(me, me_dest, ratchet, announces[ message['source_hash'] ], reply))
+            else:
+                print(f"  Cannot reply - no announce for {message['source_hash'].hex()}")
+
+
+        else:
+            print("  Could not decrypt")
+
+
 async def handle_incoming(websocket):
     async for message in websocket:
         try:
             packet = rns.decode_packet(message)
-            # print('Packet', packet)
+
             if packet['packet_type'] == rns.PACKET_ANNOUNCE:
-                print(f"ANNOUNCE from {packet['destination_hash'].hex()}")
-                announce = rns.announce_parse(packet)
-                if announce['valid']:
-                    dest = packet['destination_hash']
-                    announces[packet['destination_hash']] = announce
-                    announces[packet['destination_hash']]['destination_hash'] = packet['destination_hash']
-                    print(f"  Saved ({len(announces)}) announce from {packet['destination_hash'].hex()}")
+                await handle_announce(packet)
 
             elif packet['packet_type'] == rns.PACKET_PROOF:
-                print(f"PROOF for message {packet['destination_hash'].hex()}")
+                await handle_proof(packet)
 
             elif packet['packet_type'] == rns.PACKET_DATA:
-                message_id = rns.get_message_id(packet)
-                print(f"DATA ({message_id.hex()}) for {packet['destination_hash'].hex()}")
-                
-                # it was for me?
-                if packet['destination_hash'] == me_dest:
-                    # tell them I got it
-                    print(f"sending PROOF ({message_id.hex()})")
-                    await websocket.send(rns.build_proof(me, packet, message_id))
+                await handle_data(packet, websocket)
 
-                    # Decrypt the message
-                    plaintext = rns.message_decrypt(packet, me, [ratchet])
-                    if plaintext:
-                        message = rns.parse_lxmf_message(plaintext)
-                        content_text = message['content'].decode('utf-8')
-                        print(f"  From: {message['source_hash'].hex()}")
-                        print(f"  Title: {message['title'].decode('utf-8')}")
-                        print(f"  Content: {content_text}")
-
-                        # Send reply to sender
-                        if message['source_hash'] in announces:
-                            reply = {
-                                'title': 'EchoBot',
-                                'content': content_text
-                            } 
-                            await websocket.send(rns.build_lxmf_message(me, me_dest, ratchet, announces[ message['source_hash'] ], reply))
-                        else:
-                            print(f"  Cannot reply - no announce for {message['source_hash'].hex()}")
-
-
-                    else:
-                        print("  Could not decrypt")
         except Exception as e:
             print(f"Error handling packet: {e}")
             import traceback
